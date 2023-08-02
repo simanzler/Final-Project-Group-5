d7b48ada4a84b8692145ad77b5a65c1e5a99e999
juharan <juharan@mail.uc.edu>
2023-08-02 11:01:43 -0400

    update team member final project

--- Modules/FavoriteFood.cs
+++ Modules/FavoriteFood.cs
@@ -1,6 +1,17 @@
-﻿namespace Final_Project_Group_5.Modules
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Project_Group_5.Model
 {
+
     public class FavoriteFood
     {
+        [Key]
+        public int Id { get; set; }
+        public string Breakfast { get; set; }
+        public string Lunch { get; set; }
+        public string Dinner { get; set; }
+        public string Dessert { get; set; }
+
+
     }
 }
