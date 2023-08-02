9a4293720d7ec0c51b5a00cf02fb6f12d21c6e9f
juharan <juharan@mail.uc.edu>
2023-08-02 10:39:44 -0400

    Update team member final

--- Modules/TeamMember.cs
+++ Modules/TeamMember.cs
@@ -1,6 +1,17 @@
-﻿namespace Final_Project_Group_5.Modules
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Project_Group_5.Model
 {
+
     public class TeamMember
     {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Birthdate { get; set; }
+        public string CollegeProgram { get; set; }
+        public int YearinthePrrogram { get; set; }
+
     }
 }
