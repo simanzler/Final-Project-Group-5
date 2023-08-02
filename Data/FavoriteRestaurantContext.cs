b2deb449c779234806e36305a39c3de10bb31244
Dilpreet Supra <suprads@mail.uc.edu>
2023-08-02 12:51:11 -0400

    Context Commit 1

---
+++ Data/FavoriteRestaurantContext.cs
@@ -0,0 +1,17 @@
+﻿using Final_Project_Group_5.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Final_Project_Group_5.Data
+{
+    public class FavoriteFoodContext : DbContext
+    {
+        public FavoriteFoodContext(DbContextOptions<FavoriteFoodContext> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<FavoriteFood>().HasData(
+                new FavoriteFood { Id = 1, Breakfast = "Waffles", Lunch = "Turkey Sandwich", Dinner = "Pasta", Dessert = "Ice Cream"}
+                );
+        }
+    }
+}
---
+++ Data/TeamMemberContext.cs
@@ -0,0 +1,18 @@
+﻿using Final_Project_Group_5.Model;
+using Final_Project_Group_5.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Final_Project_Group_5.Data
+{
+    public class TeamMemberContext : DbContext
+    {
+        public TeamMemberContext(DbContextOptions<FavoriteFoodContext> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<TeamMember>().HasData(
+                new TeamMember { Id = 1, FirstName = "Dilpreet", LastName = "Supra", Birthdate = 09/05, CollegeProgram = "Software Development", YearintheProgram = 2}
+                );
+        }
+    }
+}
