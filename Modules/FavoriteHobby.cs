9d984f9ff3fbd2375a2f3b1f66223176bbb6f70e
juharan <juharan@mail.uc.edu>
2023-08-02 10:49:31 -0400

    Updat team memeber project

--- Modules/FavoriteHobby.cs
+++ Modules/FavoriteHobby.cs
@@ -1,6 +1,17 @@
-﻿namespace Final_Project_Group_5.Modules
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Project_Group_5.Model
 {
-    public class FavoriteHobby
+
+    public class Hobby
     {
+        [Key]
+        public int Id { get; set; }
+        public string Sport { get; set; }
+        public string Crafts { get; set; }
+        public int Time { get; set; }
+        public string Movie { get; set; }
+
+
     }
 }
